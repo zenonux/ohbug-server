b87675cb866c44293b4774fe13c8831d7db23cdc
chenyueban <jasonchan0527@gmail.com>
2020-08-07 18:40:44 +0800

    feat(dashboard): 不同环境下 invite/url 返回的链接 host 不同

--- packages/dashboard/src/api/invite/invite.service.ts
+++ packages/dashboard/src/api/invite/invite.service.ts
@@ -17,7 +17,23 @@ import {
   GetInviteDto,
 } from './invite.dto';
 
-const host = `http://localhost:8888`;
+const hostMap = [
+  {
+    env: 'development',
+    host: 'http://localhost:8888',
+  },
+  {
+    env: 'production',
+    host: 'https://app.ohbug.net',
+  },
+  {
+    env: 'test',
+    host: 'https://test.app.ohbug.net',
+  },
+];
+const host =
+  hostMap.find(({ env }) => process.env.NODE_ENV === env).host ||
+  'https://app.ohbug.net';
 const expire = 14;
 
 @Injectable()
