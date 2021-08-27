1cabf696d2af935d07435db327bfa847590f3148
chenyueban <jasonchan0527@gmail.com>
2021-08-27 21:42:05 +0800

    feat(app): 优化 overview 页样式

--- packages/app/src/pages/overview/Components/ProjectCard/ProjectCard.tsx
+++ packages/app/src/pages/overview/Components/ProjectCard/ProjectCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import dayjs from 'dayjs'
-import { Card } from 'antd'
+import { Card, Button, Image } from 'antd'
+import { SettingOutlined } from '@ant-design/icons'
 import clsx from 'clsx'
 
 import type { Project } from '@ohbug-server/types'
@@ -9,6 +10,8 @@ import { useRequest, usePersistFn } from '@/hooks'
 import { navigate, useModel } from '@/ability'
 import * as api from '@/api'
 import { MiniChart } from '@/components'
+
+import jsPlatform from '@/static/images/js.platform.png'
 
 import styles from './ProjectCard.module.less'
 
@@ -45,10 +48,20 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project, active }) => {
         [styles.active]: active,
       })}
       hoverable
-      title={project.name}
+      title={
+        <div className={styles.title}>
+          <div className="flex items-center">
+            <Image className="w-8 mr-3" src={jsPlatform} preview={false} />
+            <span>{project.name}</span>
+          </div>
+          <Button icon={<SettingOutlined />} type="text" />
+        </div>
+      }
       onClick={handleToIssue}
     >
-      <MiniChart trend="14d" loading={loading} data={data.buckets} />
+      <div className={styles.wrapper}>
+        <MiniChart trend="14d" loading={loading} data={data.buckets} />
+      </div>
     </Card>
   )
 }
